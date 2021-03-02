9e32627cdf0b2761c2b5b259bc52742072ef9fd6
tim099 <tim11251994@gmail.com>
2021-03-02 17:33:21 +0800

    Add UCL_FindReferencesWindow.cs

---
+++ Editor/UCL_FindReferencesWindow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+namespace UCL.CompilerLib
+{
+    public class UCL_FindReferencesWindow : EditorWindow
+    {
+        [MenuItem("Assets/Find References", false, 20)]
+        static public void ShowWindow()
+        {
+            EditorWindow.GetWindow<UCL_FindReferencesWindow>();
+        }
+    }
+}
+
