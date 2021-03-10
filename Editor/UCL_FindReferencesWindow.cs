b917271caf36d2c3b8796a3a9abd255d82590726
tim099 <tim11251994@gmail.com>
2021-03-10 12:16:41 +0800

    Update UCL_FindReferencesWindow.cs

--- Editor/UCL_FindReferencesWindow.cs
+++ Editor/UCL_FindReferencesWindow.cs
@@ -9,10 +9,39 @@ namespace UCL.CompilerLib
 {
     public class UCL_FindReferencesWindow : EditorWindow
     {
+        List<Object> m_ReferencesList = new List<Object>();
         [MenuItem("UCL/Tools/Find References", false, 20)]
+        [MenuItem("Assets/UCL Tools/Find References", false, 20)]
         static public void ShowWindow()
         {
             EditorWindow.GetWindow<UCL_FindReferencesWindow>();
+        }
+        public void Search()
+        {
+            m_ReferencesList.Clear();
+        }
+        void OnGUI()
+        {
+            EditorGUILayout.BeginVertical();
+            EditorGUILayout.BeginHorizontal();
+            Selection.activeObject = EditorGUILayout.ObjectField("Target Object:", Selection.activeObject, typeof(Object), true);
+            if (Selection.activeObject != null)
+            {
+                if (GUILayout.Button("Search",GUILayout.Width(80)))
+                {
+                    Search();
+                }
+            }
+
+            EditorGUILayout.EndHorizontal();
+
+
+
+            //GUILayout.Label("Reference Result :");
+            //foreach (Object obj in _referenceObjectList)
+                //EditorGUILayout.ObjectField(obj, typeof(Object), true);
+            EditorGUILayout.EndVertical();
+            if (Event.current.type == EventType.Repaint) Repaint();
         }
     }
 }
