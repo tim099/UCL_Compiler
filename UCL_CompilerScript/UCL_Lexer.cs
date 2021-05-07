a1f006bc6c6808737e3feb6b13796cf5006a4ffb
Tim Yeh <tim11251994@gmail.com>
2021-05-07 17:37:59 +0800

    Add UCL_Lexer.cs(UnFinished)

---
+++ UCL_CompilerScript/UCL_Lexer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+namespace UCL.CompilerLib
+{
+    /// <summary>
+    /// UCL_Lexer takes text and breaks it up into tokens
+    /// </summary>
+    public class UCL_Lexer
+    {
+        public class Token
+        {
+            /// <summary>
+            /// Try Parse token from iReader
+            /// return true if success
+            /// </summary>
+            /// <param name="iReader"></param>
+            /// <returns></returns>
+            virtual public bool TryParse(StringReader iReader)
+            {
+                return false;
+            }
+            public int m_ID;
+            public string m_Type;
+            public int m_Line;
+            public string m_Symbol;
+        }
+    }
+}
